16f63be1f41c4a72a2fc9f4a6d45bdeb887029de
abereham.wodajie <abereham.wodajie@ticketmaster.com>
2016-10-11 21:45:40 -0400

    created controller test class

--- src/main/java/com/michote/controller/michoteController.java
+++ src/main/java/com/michote/controller/michoteController.java
@@ -3,11 +3,22 @@
  */
 package com.michote.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author aberehamwodajie
  *
  * Oct 11, 2016
  */
+
+@RestController
 public class michoteController {
+	
+	@RequestMapping("/test")
+	public String test(){
+		
+		return "Hello from Michote";
+	}
 
 }
