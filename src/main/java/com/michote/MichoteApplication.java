a469ed2a62de7056b2a16b3d4fd91e1f0a2d914a
jtq603 <Mahder.Gebreyohans@capital.com>
2016-10-16 20:08:22 -0400

    working crud operation on a sample user and angularjs first draft

--- src/main/java/com/michote/MichoteApplication.java
+++ src/main/java/com/michote/MichoteApplication.java
@@ -2,8 +2,10 @@ package com.michote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.michote")
 public class MichoteApplication {
 
 	public static void main(String[] args) {
