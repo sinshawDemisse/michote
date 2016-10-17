444211ddd6ca25b4de623cd5edd42746353b4209
jtq603 <Mahder.Gebreyohans@capital.com>
2016-10-16 22:28:24 -0400

    All the entities and Dao with findby methods

--- src/main/java/com/michote/entity/HousePreference.java
+++ src/main/java/com/michote/entity/HousePreference.java
@@ -15,12 +15,12 @@ import javax.persistence.Table;
  * Oct 16, 2016
  */
 @Entity
-@Table(name = "HousePreference")
+@Table(name = "housePreference")
 public class HousePreference {
 
 	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long HousePreferenceId;
+    private long housePreferenceId;
 
      private String parking;
      private String dog;
@@ -30,62 +30,76 @@ public class HousePreference {
      private String furnished;
      private String smooking;
      private String wheelChairAccessible;
-     
-     
+
 	public long getHousePreferenceId() {
-		return HousePreferenceId;
+		return housePreferenceId;
 	}
+
 	public void setHousePreferenceId(long housePreferenceId) {
-		HousePreferenceId = housePreferenceId;
+		this.housePreferenceId = housePreferenceId;
 	}
+
 	public String getParking() {
 		return parking;
 	}
+
 	public void setParking(String parking) {
 		this.parking = parking;
 	}
+
 	public String getDog() {
 		return dog;
 	}
+
 	public void setDog(String dog) {
 		this.dog = dog;
 	}
+
 	public String getSex() {
 		return sex;
 	}
+
 	public void setSex(String sex) {
 		this.sex = sex;
 	}
+
 	public String getLaoundry() {
 		return laoundry;
 	}
+
 	public void setLaoundry(String laoundry) {
 		this.laoundry = laoundry;
 	}
+
 	public String getCat() {
 		return cat;
 	}
+
 	public void setCat(String cat) {
 		this.cat = cat;
 	}
+
 	public String getFurnished() {
 		return furnished;
 	}
+
 	public void setFurnished(String furnished) {
 		this.furnished = furnished;
 	}
+
 	public String getSmooking() {
 		return smooking;
 	}
+
 	public void setSmooking(String smooking) {
 		this.smooking = smooking;
 	}
+
 	public String getWheelChairAccessible() {
 		return wheelChairAccessible;
 	}
+
 	public void setWheelChairAccessible(String wheelChairAccessible) {
 		this.wheelChairAccessible = wheelChairAccessible;
 	}
-     
-     
 }
---
+++ src/main/java/com/michote/entity/Image.java
@@ -0,0 +1,19 @@
+package com.michote.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by jtq603 on 10/16/16.
+ */
+
+@Entity
+@Table(name = "image")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long imageId;
+
+    @NotNull
+    private String imageUrl;
+}
